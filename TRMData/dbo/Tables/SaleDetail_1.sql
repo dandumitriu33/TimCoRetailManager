ea415fca1562d070210671f5b72f2451074649f1
Dan Dumitriu <dandumitriu33@gmail.com>
2020-08-10 14:21:15 +0300

    Add foreign key relationships on tables

--- TRMData/dbo/Tables/SaleDetail_1.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[SaleDetail]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [SaleId] INT NOT NULL, 
-    [ProductId] INT NOT NULL, 
-    [Quantity] INT NOT NULL DEFAULT 1, 
-    [PurchasePrice] MONEY NOT NULL, 
-    [Tax] MONEY NOT NULL DEFAULT 0
-)
